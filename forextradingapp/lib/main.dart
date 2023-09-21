496dc59c0c3e6bb0f031e454067869f7ba1e6862
dadsparta <90604065+dadsparta@users.noreply.github.com>
2023-09-21 17:31:51 +0300

    onboarding!!!!

--- forextradingapp/lib/main.dart
+++ forextradingapp/lib/main.dart
@@ -1,20 +1,37 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:forextradingapp/pages/main_page/main_page_model.dart';
+import 'package:forextradingapp/utilities/samples/onboarding.dart';
+import 'package:rx_shared_preferences/rx_shared_preferences.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final RxSharedPreferences prefs = RxSharedPreferences.getInstance();
+  final bool? turnOfNotifications = await prefs.getBool("turnOfNotifications");
+  if (turnOfNotifications == null) {
+    await RxSharedPreferences.getInstance()
+        .setBool("turnOfNotifications", true);
+  }
+  final bool? authendicate = await prefs.getBool("authendicate");
+  runApp( MyApp(
+    authendicate: authendicate ?? false,
+  ));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({super.key, required this.authendicate});
+
+  final bool authendicate;
 
   @override
   Widget build(BuildContext context) {
-    return const CupertinoApp(
+    return CupertinoApp(
       title: 'Flutter Demo',
-      home: MainPage(),
+      initialRoute: authendicate ? "/main" : "/",
       debugShowCheckedModeBanner: false,
+      routes: {
+        "/": (context) => const SplashScreen(),
+        "/main": (context) => const MainPage(),
+      },
     );
   }
 }
