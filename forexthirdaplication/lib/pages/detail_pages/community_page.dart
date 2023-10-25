7401594f918e1c04abaa610f2113607a1bb41392
dadsparta <90604065+dadsparta@users.noreply.github.com>
2023-10-25 14:41:43 +0300

    sds

--- forexthirdaplication/lib/pages/detail_pages/community_page.dart
+++ forexthirdaplication/lib/pages/detail_pages/community_page.dart
@@ -16,7 +16,7 @@ class _State extends State<CommunityDetailPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: firstColor,
+        backgroundColor: firstsColor,
         leading: IconButton(
           onPressed: () {
             return Navigator.pop(context);
